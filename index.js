18a04e67b70f2122e02a319dfd3171b196a26527
wildenali <wildenwildenaliali@gmail.com>
2020-09-15 14:25:05 +0700

    Membuat route and how to use it

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import userRoutes from "./routes/users.js";
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Helo from Homepage");
