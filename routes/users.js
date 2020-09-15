dfa30f89c3b0ea149c51b9483687f11aca0aad10
wildenali <wildenwildenaliali@gmail.com>
2020-09-15 14:28:36 +0700

    Coba pakai data json

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,16 @@ const users = [
     lastName: "Doe",
     age: 25,
   },
+  {
+    name: "Jane",
+    lastName: "Doe",
+    age: 22,
+  },
 ];
 
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
+  console.log(users);
   res.send(users);
 });
 
