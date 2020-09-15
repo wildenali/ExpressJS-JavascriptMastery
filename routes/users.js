238c4e7af5cb53e51048e540e386fc10eb0f1f90
wildenali <wildenwildenaliali@gmail.com>
2020-09-15 14:27:47 +0700

    Coba pakai data json

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+  {
+    name: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+];
+
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
-  res.send("Helow");
+  res.send(users);
 });
 
 export default router;
